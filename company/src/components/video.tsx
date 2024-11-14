319599f51582c98834a7af84903701567ae6c761
“NaufalNurAdrian” <nuradriannaufal@gmail.com>
2024-11-14 19:43:34 +0700

    company profile 3.6

--- company/src/components/video.tsx
+++ company/src/components/video.tsx
@@ -1,20 +1,8 @@
-'use client';
-
 import { getVideo } from "@/libs/contentful";
 import { IVideo } from "@/types/video";
-import { useEffect, useState } from "react";
 
-export default function Video() {
-  const [data, setData] = useState<IVideo[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const videos = await getVideo();
-      setData(videos);
-    };
-    fetchData();
-  }, []);
-
+export default async function Video() {
+  const data: IVideo[] = await getVideo()
   return (
     <div className="p-20 bg-slate-100">
       {data.map((item, idx) => {
@@ -24,9 +12,7 @@ export default function Video() {
               src={`https:${item.fields.introduction.fields.file.url}`}
               muted={false}
               controls
-              preload="metadata"
               className="video-player w-full rounded-xl"
-              onError={() => console.error("Video failed to load")}
             />
           </div>
         );
