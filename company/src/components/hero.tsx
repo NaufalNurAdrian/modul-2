358bbab6db8ea75dd69ea2ffbfc25bbe3f0f5704
“NaufalNurAdrian” <nuradriannaufal@gmail.com>
2024-11-14 20:06:49 +0700

    company profile 3.8

--- company/src/components/hero.tsx
+++ company/src/components/hero.tsx
@@ -3,7 +3,7 @@ export default function Hero() {
       <section className="flex flex-col items-center justify-center w-full h-[60vh] md:h-[80vh] lg:h-screen bg-slate-100 px-4 text-center">
         <h3 className="text-sm md:text-base font-light mb-4">Empowering Tomorrow</h3>
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
-          Naufal Technology's
+          Naufal Technology
         </h1>
         <h2 className="text-md md:text-lg font-light">
           With Cutting-Edge Technology
